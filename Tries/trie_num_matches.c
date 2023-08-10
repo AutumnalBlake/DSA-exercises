e0c307f661fe784500fc8887bcc6e232e0ea4720
Blake Morris <bmorri9999@gmail.com>
2023-08-10 15:36:59 +1000

    trie-num-matches

---
+++ Tries/trie_num_matches.c
@@ -0,0 +1,28 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include "trie.h"
+
+int TrieNumMatches(TrieNode t, char *word) {
+    // TODO
+    return 0;
+}
+
+int main() {
+    TrieNode t = TrieNew();
+
+    TrieInsert(t, "APPLES");
+    TrieInsert(t, "APPLE");
+    TrieInsert(t, "APPLY");
+    TrieInsert(t, "ABLE");
+    TrieInsert(t, "ALE");
+    TrieInsert(t, "BALE");
+
+    TrieShow(t);
+
+    assert(TrieNumMatches(t, "A") == 5);
+    assert(TrieNumMatches(t, "APPLE") == 2);
+    assert(TrieNumMatches(t, "ALTER") == 0);
+  
+    TrieFree(t);
+}
